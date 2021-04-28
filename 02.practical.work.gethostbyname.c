fffcf5435688130c0008291703634cdc8acab84a
Dang Hoang Phuc <phucdh.ba9050@st.usth.edu.vn>
2021-04-29 00:12:42 +0700

    submit code lab 2

---
+++ 02.practical.work.gethostbyname.c
@@ -0,0 +1,41 @@
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h> 
+#include <errno.h>
+#include <arpa/inet.h>
+
+int main(int argc, char const *argv[])
+{
+	char hostname[256];
+	if(argc == 1){
+		printf("Enter a hostname: ");
+		scanf("%s", hostname);		
+
+	}
+	else if(argc == 2)
+	{
+		char *hostname = argv[1];
+	}
+	else if(argc > 2)
+	{
+		printf("There are too many results! \n");
+		exit(-1);
+	}
+	else if(argc < 2)
+	{
+		printf("I checked it, but found nothing.\n");
+		exit(1);
+	}
+
+	struct hostent *gethost = gethostbyname(hostname);
+	struct in_addr **IP;
+	IP = (struct in_addr **)gethost -> h_addr_list;
+
+	for(int i=0; IP[i] != NULL; i++){
+		printf("%s\n", inet_ntoa(*IP[i]));
+	}
+
+	return 0;
+}
