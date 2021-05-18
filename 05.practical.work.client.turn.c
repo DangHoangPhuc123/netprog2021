0ce9bb90f0a49fd614f6d8be4c9436a928282fe0
Dang Hoang Phuc <phucdh.ba9050@st.usth.edu.vn>
2021-05-18 10:10:25 +0700

    submit labwork5

--- 05.practical.work.client.turn.c
+++ 05.practical.work.client.turn.c
@@ -1,10 +1,3 @@
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
 
 #include <arpa/inet.h>
 #include <netdb.h>
